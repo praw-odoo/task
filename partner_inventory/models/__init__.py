2b584fb1a3911c1c606f4e897de5719dc617a2a4
Pratik Awasthi <praw@odoo.com>
2022-03-02 14:23:36 +0530

    task commit

--- partner_inventory/models/__init__.py
+++ partner_inventory/models/__init__.py
@@ -1,3 +1,4 @@
 from . import res_partner
 from . import product_list
 from . import sale_order_line
+from . import account_move_line
---
+++ project_task_subtask/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'project_task_subtask',
+    'version': '1.0',
+    'category': 'Tools',
+    'sequence': 15,
+    'summary': 'modify project_task_subtask',
+    'description': "modify task 6",
+    'website': 'http://www.odoo.com/task8',
+    'depends': ['project'],
+    'data':[
+        'views/project_task_views.xml',
+    ],
+    'demo': [],
+    #'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
