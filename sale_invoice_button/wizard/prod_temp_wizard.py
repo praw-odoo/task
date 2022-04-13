e7f845432e30bb26940978868c6ed96f17125477
Pratik Awasthi <praw@odoo.com>
2022-04-13 16:01:06 +0530

    [UPD] sale_invoice_button : controller updated

--- sale_invoice_button/wizard/prod_temp_wizard.py
+++ sale_invoice_button/wizard/prod_temp_wizard.py
@@ -10,43 +10,44 @@ class ProdTemp(models.TransientModel):
     field declaration
     """
     move_id = fields.Many2one("account.move", string="Invoice")
-    product_id = fields.Many2one("product.template", string="Product",
-    domain="[('is_insurance', '=', True)]" )
+    product_id = fields.Many2one(
+        "product.template", string="Product", domain="[('is_insurance', '=', True)]"
+    )
     insurance = fields.Float(string="Tax %", compute="_compute_insurance")
 
     @api.depends("product_id")
     def _compute_insurance(self):
         for record in self:
-            record.insurance = record.product_id.percent_applicable
+
+            record.insurance = (
+                record.product_id.percent_applicable * self.move_id.amount_untaxed
+            )
 
     # method for creating request for quotation
     def request_for_invocing(self):
         """
-        this method create add product from the wizard
+        this method add product from the wizard
         """
-
+        taxes = self.product_id.taxes_id.filtered(
+            lambda tax: tax.company_id == self.move_id.company_id
+        )
+        if taxes and self.move_id.fiscal_position_id:
+            taxes = self.move_id.fiscal_position_id.map_tax(taxes)
+        print("\n\n\n taxes ", taxes.id)
         val_list = {
-            "product_id": self.product_id.id,
-            "name": self.product_id.name,
-            "quantity": 1,
-            "product_uom_id": self.product_id.uom_id.id,
-            "account_id": self.move_id.journal_id.default_account_id.id,
-            'tax_ids' : self.product_id.taxes_id,
-            "price_unit": self.product_id.percent_applicable,
-            "move_id": self.move_id.id,
-            # "price_subtotal" : self.product_id.percent_applicable
+            "invoice_line_ids": [
+                (0,0,{
+                        "product_id": self.product_id.id,
+                        "name": self.product_id.name,
+                        "quantity": 1,
+                        "product_uom_id": self.product_id.uom_id.id,
+                        "account_id": self.move_id.journal_id.default_account_id.id,
+                        "tax_ids": taxes.ids,
+                        "price_unit": self.insurance,
+                        "move_id": self.move_id.id,
+                    },
+                )
+            ],
         }
-        # chk = 0
-        # for move in self.move_id:
-        #     for line in move.line_ids:
-        #         if line.debit and chk != 1:
-        #             try:
-        #                 line.debit = line.debit + self.product_id.percent_applicable
-        #                 chk = chk +1
-        #             except:
-        #                 self.env["account.move.line"].create(val_list)
-        #                 print("\n\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa",line.debit)
-        # print("\n\n\n self.move_id.debit",self.record.move_id.line_ids.debit)
-        # self.move_id.invoice_line_ids.debit = self.move_id.invoice_line_ids.debit + self.product_id.percent_applicable
-        self.move_id._validate_taxes_country()
-        self.env["account.move.line"].create(val_list)
+
+        self.move_id.write(val_list)
--- website_payroll/controllers/main.py
+++ website_payroll/controllers/main.py
@@ -1,9 +1,7 @@
 
-
 from odoo import http
 from odoo.http import request
 from odoo.http import Controller
-
 from odoo.addons.portal.controllers import portal
 from odoo.addons.portal.controllers.portal import pager as portal_pager, get_records_pager
 
@@ -16,73 +14,61 @@ class CustomerPortal(Controller):
 
         Does not include the record counts.
         """
-        # get customer sales rep
-        sales_user = False
+        payslip_user = False
         partner = request.env.user.partner_id
         if partner.user_id and not partner.user_id._is_public():
-            sales_user = partner.user_id
+            payslip_user = partner.user_id
 
         return {
-            'sales_user': sales_user,
+            'payslip_user': payslip_user,
             'page_name': 'home',
         }
+
+    def _prepare_payslips_domain(self, partner):
+        return [
+            # ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+            ('state', 'in', ['draft', 'done', 'paid'])
+        ]
+
     @http.route(['/my/payslip', '/my/payslip/page/<int:page>'],  type='http', auth="user", website=True)
     def portal_my_payslips(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, **kw):
-        # values = self._prepare_portal_layout_values()
+        values = self._prepare_portal_layout_values()
         partner = request.env.user.partner_id
-        print("\n\n partner",partner)
-        # HrPayslip = request.env['hr.payslip']
-        # # if not sortby:
-        # #     sortby = 'date'
-        # if date_begin and date_end:
-        #     domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
-        # slip_count = HrPayslip.search_count(domain)
-        # pager = portal_pager(
-        #     url="/my/orders",
-        #     url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
-        #     total=slip_count,
-        #     page=page,
-        #     step=self._items_per_page
-        # )
-        return request.render("website_payroll.portal_my_payslips")
+        HrPayslip = request.env['hr.payslip']
+        domain = self._prepare_payslips_domain(partner)
+        if not sortby:
+            sortby = 'date'
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+        slip_count = HrPayslip.sudo().search_count([])
+        pager = portal_pager(
+            url="/my/payslip",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=slip_count,
+            page=page,
+            # step=self._items_per_page
+        )
 
-        # values = self._prepare_portal_layout_values()
-        # partner = request.env.user.partner_id
-        # SaleOrder = request.env['sale.order']
+        slips = HrPayslip.sudo().search([])
+        # request.session['my_orders_history'] = orders.ids[:100]
 
-        # domain = self._prepare_orders_domain(partner)
+        values = ({
+            'date': date_begin,
+            'orders': slips.sudo(),
+            'page_name': 'slip',
+            'pager': pager,
+            'default_url': '/my/payslip',
+            # 'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+        })
+        return request.render("website_payroll.portal_my_payslips",values)
+
 
         # searchbar_sortings = self._get_sale_searchbar_sortings()
 
-        # # default sortby order
-        # if not sortby:
-        #     sortby = 'date'
         # sort_order = searchbar_sortings[sortby]['order']
 
         # if date_begin and date_end:
         #     domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
 
-        # # count for pager
-        # order_count = SaleOrder.search_count(domain)
-        # # pager
-        # pager = portal_pager(
-        #     url="/my/orders",
-        #     url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
-        #     total=order_count,
-        #     page=page,
-        #     step=self._items_per_page
-        # )
-        # # content according to pager
-        # orders = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
-        # request.session['my_orders_history'] = orders.ids[:100]
-
-        # values.update({
-        #     'date': date_begin,
-        #     'orders': orders.sudo(),
-        #     'page_name': 'order',
-        #     'pager': pager,
-        #     'default_url': '/my/orders',
-        #     'searchbar_sortings': searchbar_sortings,
-        #     'sortby': sortby,
-        # })
         # return request.render("sale.portal_my_orders", values)
